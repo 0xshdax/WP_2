86b0c298d3542b1178803db5ef64044eb2e3f2d2
Rafshanzani Suhada <12192506@bsi.ac.id>
2025-09-25 21:53:22 +0700

    New

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themeum/tutor',
+        'pretty_version' => '3.8.0.x-dev',
+        'version' => '3.8.0.9999999-dev',
+        'reference' => '9ab43fef50acd3c51f724d07bb300637ac71fccf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'themeum/tutor' => array(
+            'pretty_version' => '3.8.0.x-dev',
+            'version' => '3.8.0.9999999-dev',
+            'reference' => '9ab43fef50acd3c51f724d07bb300637ac71fccf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
